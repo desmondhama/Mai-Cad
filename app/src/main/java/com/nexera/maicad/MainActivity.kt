82e1ea2a47169b50d63b4a08bd2d1b72bc29663b
desmondhama <desmondhama@gmail.com>
2025-03-11 21:55:51 +0200

    Update MainActivit,res/drawable,res/values

--- app/src/main/java/com/nexera/maicad/MainActivity.kt
+++ app/src/main/java/com/nexera/maicad/MainActivity.kt
@@ -11,6 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.nexera.maicad.ui.theme.MyComposeApplicationTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
 
@@ -21,22 +29,38 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    TitleComposable()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyComposeApplicationTheme {
-        Greeting("Android")
-    }
+fun TitleComposable(modifier: Modifier = Modifier) {
+    Column(
+		
+	) {
+	    Image(
+		painter = painterResource(id = R.drawable.im_android),
+		contentDescription = null,
+		modifier = Modifier.fillMaxSize(0.2f)
+		)
+		Text(
+		text = stringResource(id = R.string.full_name),
+		modifier = Modifier
+			.padding(8.dp)
+			.fillMaxWidth(),
+		style = TextStyle(
+		    fontWeight = FontWeight.Bold,
+			fontSize = 24.sp
+		    )
+		)
+		Text(
+		text = stringResource(id = R.string.title),
+		modifier = Modifier.fillMaxWidth(),
+		style = TextStyle(
+		    fontSize = 18.sp,
+		    )
+		) 
+	}
 }
